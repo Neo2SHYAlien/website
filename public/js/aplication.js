756061f102af18242bacc2a5396e12db46dd3b64
Leon Anavi <leonanavi@gmail.com>
2013-10-09 09:19:20 +0300

    Basic intergration of WhoIsInTheLab

--- public/js/aplication.js
+++ public/js/aplication.js
@@ -23,5 +23,54 @@ $(document).ready(function(){
 	$('a[rel*=external]').click( function() {
 		window.open(this.href);
 		return false;
-	});		
+	});	
+
+	whoIsInTheLab();	
 });
+
+function whoIsInTheLab()
+{
+	$.ajax({
+		url: 'http://raspberry.hackafe.org/?format=json',
+		dataType: 'jsonp',
+		success: function(data){
+			if ('undefined' === typeof data.data)
+			{
+				return;
+			}
+			var nCount = data.data.count;
+			var sText = '';
+			if (0 >= nCount)
+			{
+				sText = 'В лаба няма никой.';
+			}
+			else
+			{
+				var sText = '<a href="http://raspberry.hackafe.org/" class="info" target="_blank">В лаба са: ';
+				var sUsers = '';
+				for(var nUser=0; nUser<data.data.users.length; nUser++)
+				{
+					var sPrefix = (0 == nUser) ? '' : ', ';
+					sUsers += sPrefix+data.data.users[nUser].name1;
+				}
+				sText += sUsers;
+				var nGuests = data.data.guests;
+				if (0 >= nGuests)
+				{
+					sText += '.';
+				}
+				else
+				{
+					if (0 < sUsers.length)
+					{
+						sText += ' и ';
+					}
+					sText += nGuests;
+					sText += (1 == nGuests) ? ' гостуващо устройство.' : ' гостуващи устройства.';
+				}
+				sText += '</a>';
+			}
+			$('#whoIsInTheLab').html(sText);
+		}
+	});
+}
