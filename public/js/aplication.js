b914f723d57a0886346e21dc81a41682f542f229
Leon Anavi <leonanavi@gmail.com>
2013-10-11 01:03:19 +0300

    Simple jQuery-UI dialog to display list of users at the lab was added

--- public/js/aplication.js
+++ public/js/aplication.js
@@ -1,3 +1,10 @@
+$(function() {
+	$( "#whoIsInTheLabDialog" ).dialog({
+		modal: true,
+		autoOpen: false
+	});
+});
+
 $(document).ready(function(){
 	//Start scroll
 	$('a[href^="#"]').on('click',function (e) {
@@ -39,28 +46,65 @@ function whoIsInTheLab()
 				return;
 			}
 			var nCount = data.data.count;
+			var nGuests = data.data.guests;
 			var sText = '';
 			if (0 >= nCount)
 			{
 				sText = 'В лаба няма никой.';
+				$('#whoIsInTheLabList').html(sText);
 			}
 			else
 			{
-				var sText = '<a href="http://raspberry.hackafe.org/" class="info" target="_blank">В лаба са: ';
+				var sText = '<a href="#" onclick="showWhoIsInTheLab()" class="info">В лаба са: ';
 				var sUsers = '';
+				var sList = '';
+				var users = data.data.users;
 				for(var nUser=0; nUser<data.data.users.length; nUser++)
 				{
 					var sPrefix = (0 == nUser) ? '' : ', ';
-					sUsers += sPrefix+data.data.users[nUser].name1;
+					sUsers += sPrefix+users[nUser].name1;
+					
+					var sUserImage = '';
+					var sFacebookInfog = '';
+					if ( ("undefined" != users[nUser].facebook) && (0 < users[nUser].facebook.length) )
+					{
+						sFacebookInfo = '<a href="'+users[nUser].facebook+'" target="_blank">Facebook</a> ';
+						sUserImage = 'http://graph.facebook.com/'+users[nUser].facebook+'/picture?type=normal';
+					}
+					
+					//load data about the users
+					sList += '<li>';
+					sList += '<p><div class="whoTitle"><a href="'+users[nUser].website+'" target="_blank">';
+					sList += users[nUser].name1+' '+users[nUser].name2+'</a></div>';
+					sList += '<div class="whoDetails">';
+					sList += sFacebookInfo;
+					if ( ("undefined" != users[nUser].twitter) && (0 < users[nUser].twitter.length) )
+					{
+						sList += '<a href="'+users[nUser].twitter+'" target="_blank">Twitter</a> ';
+					}
+					if ( ("undefined" != users[nUser].googlePlus) && (0 < users[nUser].googlePlus.length) )
+					{
+						sList += '<a href="'+users[nUser].googlePlus+'" target="_blank">Google+</a> ';
+					}
+					if ( ("undefined" != users[nUser].email) && (0 < users[nUser].email.length) )
+					{
+						sList += '</br>e-mail: <a href="mailto:'+users[nUser].email+'" target="_blank">'+users[nUser].email+'</a> ';
+					}
+					if ( ("undefined" != users[nUser].tel) && (0 < users[nUser].tel.length) )
+					{
+						sList += '</br>tel: <a href="'+users[nUser].tel+'" target="_blank">'+users[nUser].tel+'</a> ';
+					}
+					sList += '</div></p></li>';
 				}
+								
 				sText += sUsers;
-				var nGuests = data.data.guests;
 				if (0 >= nGuests)
 				{
 					sText += '.';
 				}
 				else
 				{
+					sList += '<p class="whoGuests">Гостуващи устройства: '+nGuests+'</p>';
 					if (0 < sUsers.length)
 					{
 						sText += ' и ';
@@ -69,8 +113,16 @@ function whoIsInTheLab()
 					sText += (1 == nGuests) ? ' гостуващо устройство.' : ' гостуващи устройства.';
 				}
 				sText += '</a>';
+				
+				$('#whoIsInTheLabList').html(sList);
 			}
 			$('#whoIsInTheLab').html(sText);
 		}
 	});
+}
+
+function showWhoIsInTheLab()
+{
+	window.scrollTo(0, 0);
+	$( "#whoIsInTheLabDialog" ).dialog( "open" );
 }
