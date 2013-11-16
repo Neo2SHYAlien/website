62192f8766e298d5de8a74d43870c635e2357bd6
echeveria <s.p.germanov@gmail.com>
2013-11-16 16:15:06 +0200

    Ufff

---
+++ lib/Hackafe/Models/Events.php
@@ -0,0 +1,16 @@
+<?
+
+	//use Hackafe\DB\Mysqli;
+
+	class Hackafe_Models_Events {
+
+		private $db;
+
+		public function __construct() {
+			//$this->db = Mysqli::singleton();
+			//$this->_temp_create_table(); // COMMENT THIS LINE AFTER FIRST RUN :)
+		}
+
+		
+
+	}
